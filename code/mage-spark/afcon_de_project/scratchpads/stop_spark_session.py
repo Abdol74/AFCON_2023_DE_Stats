934b25c9a5fa6b1d2db436dddc55565aeeb58194
Abdelrahman <boda56901@gmail.com>
2024-04-07 03:13:12 +0000

    setup new architecture for our project uisng terraform
    make mage-spark image as docker-compose
    
    implement elt srcript to load data from api to data lake

--- code/mage-spark/afcon_de_project/scratchpads/stop_spark_session.py
+++
@@ -1,7 +0,0 @@
-
-def stop_spark_session(**kwargs):
-    spark = kwargs.get('spark')
-    print(spark)
-    if spark:
-        spark.stop()
-    print(spark)
---
+++ mage/data_engineering_afcon_2023/data_exporters/load_afcon_events_to_datalake.py
@@ -0,0 +1,48 @@
+from mage_ai.settings.repo import get_repo_path
+from mage_ai.io.config import ConfigFileLoader
+from mage_ai.io.google_cloud_storage import GoogleCloudStorage
+from pandas import DataFrame
+from os import path
+import os
+if 'data_exporter' not in globals():
+    from mage_ai.data_preparation.decorators import data_exporter
+
+
+
+
+@data_exporter
+def export_data_to_google_cloud_storage(dfs: list[DataFrame], **kwargs) -> None:
+
+    
+    config_path = path.join(get_repo_path(), 'io_config.yaml')
+
+
+    config_profile = 'default'
+    project_id = os.environ['PROJECT_ID']
+    bucket_name = os.environ['BUCKET_NAME']
+    
+
+    for df in dfs:
+        print(df['source'][0])
+        if df['source'][0] == 'competitions':
+            object_key = '{}.parquet'.format(df['source'][0])
+            GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            df,
+            bucket_name,
+            object_key,)
+
+        elif df['source'][0] == 'acfon_matches':
+
+            object_key = '{}.parquet'.format(df['source'][0])
+            GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            df,
+             bucket_name,
+            object_key,)
+        
+        elif df['source'][0] == 'match_events':
+
+            object_key = '{}.parquet'.format(df['source'][0])
+            GoogleCloudStorage.with_config(ConfigFileLoader(config_path, config_profile)).export(
+            df,
+             bucket_name,
+            object_key,)
